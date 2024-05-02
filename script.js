5dfcb9eff7368047f5c0f0f9e26b68961cb68c8d
Sabine Emden <EmdenSabine@gmail.com>
2024-05-02 17:36:16 -0400

    Add functions for basic math operators

---
+++ script.js
@@ -0,0 +1,17 @@
+// functions for basic math operators
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return "NOPE!";
+  return a / b;
+}
